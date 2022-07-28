4b84cc900a77a99daaf765a4acc95bd186f3f0d6
Virginia <vbernasconi@altimetrik.com>
2022-07-28 19:13:00 -0300

    Add feature reset the game

--- starter/script.js
+++ starter/script.js
@@ -13,21 +13,31 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-//Create "hidden" class in CSS
-
-//starting conditions:
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let currentScore = 0;
-const scores = [0, 0];
-let activePlayer = 0;
-let playing = true;
 const switchPlayer = function () {
   activePlayer = activePlayer === 0 ? 1 : 0;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+//starting conditions:
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  diceEl.classList.add('hidden');
+};
+init();
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
@@ -72,3 +82,5 @@ btnHold.addEventListener('click', function () {
     switchPlayer();
   }
 });
+
+btnNew.addEventListener('click', init);
